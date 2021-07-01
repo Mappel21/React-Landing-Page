e1806a47f233a83432b656a5a2dc1a9daf3ca04c
Mappel21 <Mappel7676@gmail.com>
2021-07-01 00:20:09 +0000

    footer

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,30 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Navbar from "./navbar";
+import Jumbotron from "./jumbotron";
+import Cards from "./card";
+import Footer from "./footer";
 
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<Navbar />
+			<div className="container-fluid mt-4 mx-auto px-5">
+				<Jumbotron />
+				<div className="container-fluid mb-4 mx-auto px-5 mw-100 ">
+					<div className="card-deck">
+						{array &&
+							array.map(element => (
+								<Cards
+									key={element.title}
+									title={element.image}
+									discription={element.discription}
+								/>
+							))}
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</>
 	);
 }
